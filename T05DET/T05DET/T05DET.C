0edfa66d858dd77317f28d2834836a81f23fa965
Kirillova Arina (AK5) <horsegrand@yandex.ru>
2017-06-06 10:56:42 +0300

    release

--- T05DET/T05DET/T05DET.C
+++ T05DET/T05DET/T05DET.C
@@ -8,10 +8,13 @@
 #include <time.h>
 
 #define max 42
-#define mtrMax 4
+#define mtrMax 42
 
-int p[max], mtr[mtrMax][mtrMax];
-int n = mtrMax, parity = 0, det;
+#pragma warning(disable: 4244)
+
+int p[max];
+int n, parity = 0;
+double m[mtrMax][mtrMax], det = 0;
 
 /* function swap */
 void swap( int* a, int* b )
@@ -21,38 +24,21 @@ void swap( int* a, int* b )
   *b = tmp;
 }/* End of swap */
 
-/* function PrintPerm */
-void PrintPerm( void )
-{
-  int i;
-  FILE* F;
-
-  F = fopen("PERM.TXT", "a");
-
-  if (F == NULL)
-  {
-    printf("ERROR cannot open file\n");
-    return;
-  }
-
-  for (i = 0; i < n; i++)
-    fprintf(F, "%i", p[i]);
-  fprintf(F, " - %s\n", parity ? "odd" : "even");
-
-  fclose(F);
-}/* End of PrintPerm */
-
 /* function Go */
 /* ! RECURSIVE ! */
 void Go( int pos )
 {
-  int i, tmp, saveParity;;
+  int i, saveParity;
+  double tmp = 1;
 
   if (pos >= n)
   {
-    //PrintPerm();
+    for (i = 0; i < n; i++)
+      tmp *= m[i][p[i]];
+    det+= !parity ? tmp : -tmp;
     return;
   }
+
 
   Go(pos + 1);
   saveParity = parity;
@@ -65,21 +51,20 @@ void Go( int pos )
   }
 
   tmp = p[pos];
-  for (i = pos + 1; i <n; i++)
+  for (i = pos + 1; i < n; i++)
     p[i - 1] = p[i];
   p[n - 1] = tmp;
   parity = saveParity;
 
 }/* End of Go */
 
-/* function main */
-void main( void )
+/* function MatrixLoad */
+void MatrixLoad(char* FileName)
 {
   int i, j;
-  float t;
   FILE* mF;
 
-  mF = fopen("MTR.TXT", "r");
+  mF = fopen(FileName, "r");
 
   if (mF == NULL)
   {
@@ -87,20 +72,47 @@ void main( void )
     return;
   }
 
-  for (i = 0; i < mtrMax; i++)
-    for (i = 0; i < mtrMax; i++)
-      fscanf(mF, "%i", &mtr[i][j]);
+  fscanf(mF, "%d", &n);
+
+  if (n > mtrMax)
+    n = mtrMax;
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      fscanf(mF, "%lf", &m[i][j]);
+
+  fclose(mF);
+}
+/* End of MatrixLoad */
+
+/* function main */
+void main( void )
+{
+  int i;
+  float t;
+  FILE* mF;
+
+  MatrixLoad("Matrix.txt");
+
+  mF = fopen("Matrix.txt", "a");
+  if (mF == NULL)
+  {
+    printf("ERROR cannot open file\n");
+    return;
+  }
   
   for (i = 0; i < n; i++)
     p[i] = i;
-
-
 
   t = clock();
   Go(0);
   t = clock() - t;
   t = t / CLOCKS_PER_SEC;
-  //fprintf("Timer: %f\n\n", t);
+
+
+  fprintf(mF ,"Determinator: %lf\n", det);
+
+  fclose(mF);
 
 }/* End of main */
 
